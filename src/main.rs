a8d37b831b650ea4c374b46a1d9b2b387075d668
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2022-06-05 21:03:31 +0100

    init commit
    - basic rendering

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+mod debug;
+mod future;
+
+use bevy::prelude::*;
+use bevy::render::color::Color;
+use bevy::sprite::MaterialMesh2dBundle;
+use bevy::window::PresentMode;
+use crate::debug::DebugPlugin;
+
+pub const CLEAR: Color = Color::rgb(0.1, 0.2, 0.3);
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(CLEAR))
+        .insert_resource(WindowDescriptor {
+            width: 1280.0,
+            height: 720.0,
+            title: "Planet Simulator".to_string(),
+            present_mode: PresentMode::Fifo,
+            ..default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(DebugPlugin)
+        .add_startup_system(spawn_camera)
+        .add_startup_system(spawn_planet)
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+fn spawn_planet(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
+    commands
+        .spawn_bundle(MaterialMesh2dBundle {
+            mesh: meshes.add(future::Circle::new(50.).into()).into(),
+            material: materials.add(ColorMaterial::from(Color::SEA_GREEN)),
+            ..default()
+        })
+        .insert(Name::new("Planet"));
+}
