40ecdd046c6c0ae7be64814998194f2dd4a81db3
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2022-06-06 21:16:19 +0100

    add global settings struct and debug gui for it

--- src/debug.rs
+++ src/debug.rs
@@ -1,12 +1,24 @@
 use bevy::prelude::*;
-use bevy_inspector_egui::WorldInspectorPlugin;
+use bevy_inspector_egui::{egui, InspectorPlugin, WorldInspectorPlugin};
+use bevy_inspector_egui::bevy_egui::{EguiContext, EguiPlugin};
+use bevy_inspector_egui::egui::Slider;
+use crate::GlobalSettings;
 
 pub struct DebugPlugin;
 
 impl Plugin for DebugPlugin {
     fn build(&self, app: &mut App) {
         if cfg!(debug_assertions) { //TODO make this user accessible
-            app.add_plugin(WorldInspectorPlugin::new());
+            app.add_plugin(WorldInspectorPlugin::new())
+                .add_system(ui_example)
+            ;
         }
     }
+}
+
+fn ui_example(mut egui_context: ResMut<EguiContext>, mut res_global_settings: ResMut<GlobalSettings>) {
+    let global_settings = res_global_settings.as_mut();
+    egui::Window::new("Global settings").show(egui_context.ctx_mut(), |ui| {
+        ui.add(Slider::new(&mut global_settings.meters_to_pix, 0.0..=500000.0))
+    });
 }
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,15 @@ use bevy::prelude::*;
 use bevy::render::color::Color;
 use bevy::sprite::MaterialMesh2dBundle;
 use bevy::window::PresentMode;
+use bevy_inspector_egui::Inspectable;
 use crate::debug::DebugPlugin;
 
 pub const CLEAR: Color = Color::rgb(0.1, 0.2, 0.3);
+
+#[derive(Inspectable, Default)]
+pub struct GlobalSettings {
+    meters_to_pix: f64,
+}
 
 fn main() {
     App::new()
@@ -18,6 +24,9 @@ fn main() {
             title: "Planet Simulator".to_string(),
             present_mode: PresentMode::Fifo,
             ..default()
+        })
+        .insert_resource(GlobalSettings {
+            meters_to_pix: 250000.0
         })
         .add_plugins(DefaultPlugins)
         .add_plugin(DebugPlugin)
